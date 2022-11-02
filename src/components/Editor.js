8c1027c6e731fcf83522a724b78d56683efbd55a
iamBijoyKar <bijoypoco12345@gmail.com>
2022-11-02 12:30:14 +0530

    commit before pull

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -14,7 +14,7 @@ function Editor(){
         uploadImg.classList.add('activate')
         document.querySelector('#canvas-heading').innerHTML = 'Relese to Upload'
         canvas.classList.add('activate')
-        console.log('drag over')
+        // console.log('drag over')
         event.stopPropagation();
     }
     function canvasDragEnd(event){
@@ -23,20 +23,26 @@ function Editor(){
         uploadImg.classList.remove('activate')
         document.querySelector('#canvas-heading').innerHTML = 'Drag and Drop to Upload'
         canvas.classList.remove('activate')
-        console.log('drag end')
+        // console.log('drag end')
         event.stopPropagation();
     }
     function drop(event){
-        // event.preventDefault()
+        event.preventDefault()
+        const canvas = document.querySelector('.canvas')
+        canvas.onDrop(
+        console.log('drop')
+        )
         console.log('drop')
         const file = event.dataTransfer.files[0]
         console.log(file)
+        event.stopPropagation();
+
     }
 
     return(
         <section className='section section2'>
             <div className='editor-sec' >
-                <div className='canvas' onDragOver={ canvasDragOver } onClick={ canvasClicked } onDragLeave={ canvasDragEnd } onDropCapture = { drop }>
+                <div className='canvas' onDragOver={ canvasDragOver } onClick={ canvasClicked } onDragLeave={ canvasDragEnd } onDragExit= { drop }>
                     <img src={uploadImg} alt={ 'uplaod icon' } id="upload-img" />
                     <h2 id='canvas-heading'>Drag and Drop to Upload file</h2>
                     <span>Or</span>
