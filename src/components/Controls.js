5bcaf255d1f32b0f0069ccabbfe096702fbaa6a2
iamBijoyKar <bijoypoco12345@gmail.com>
2022-11-05 21:51:21 +0530

    font size change function is added

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -12,8 +12,15 @@ function DisplayColor(event){
     const color = event.target.value
     document.querySelector('#upper-text').style.color = color
     document.querySelector('#lower-text').style.color = color
-
 }
+function FontSize(event){
+    const size = event.target.value
+    document.querySelector('#upper-text').style.fontSize = `${size}px`
+    document.querySelector('#lower-text').style.fontSize = `${size}px`
+    document.querySelector('.range-value').innerHTML = size
+}
+
+
 function Controls(){
 
 
@@ -23,6 +30,7 @@ function Controls(){
             <input type='text' id='upper-input' onChange={ DisplayTextUpper } placeholder='Enter upper text here'/>
             <input type='text' id='lower-input' onChange={ DisplayTextLower } placeholder='Enter lower text here'/>
             <label for='color-input'>Pick text color : <input type='color' onChange={ DisplayColor } id='color-input'/></label>
+            <label for='font-size-input'>Font Size : <input type='range'  min='20' max='50' id='font-size-input' onChange={ FontSize } /><p className='range-value'></p></label>
             <button id='generate-btn'>Generate</button>
         </div>
     )
